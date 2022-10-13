d8612397695eda4a042aec72ca648a4438ff738b
koto253 <koto253@outlook.com>
2022-10-13 19:35:33 -0400

    add dog year project

---
+++ Dog_year/dogYear.js
@@ -0,0 +1,19 @@
+#!/usr/bin/node
+
+// declare myAge
+let myAge = 1
+
+// declare early-year
+let earlyYears = 2
+earlyYears = earlyYears * 10.5
+
+// declare laterYears
+let laterYears = myAge - 2
+laterYears = laterYears * 4
+
+// declare and assigne myAgeInDogYears
+let myAgeInDogYears = earlyYears + laterYears
+
+let myName = 'Sparrow'.toLowerCase()
+
+console.log(`My name is ${myName}. I am ${myAge} years old in human years which is ${myAgeInDogYears} years old in dog years.`)
